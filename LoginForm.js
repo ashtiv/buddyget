ac2da6edafa165e3e144ba27b65276fe48eaa3f6
ashtiv <ashishtiwari05032002@gmail.com>
2023-02-11 21:38:17 +0530

    final

--- LoginForm.js
+++ LoginForm.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { TouchableOpacity, Image, View } from 'react-native';
+import { TouchableOpacity, Image, View, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
@@ -20,6 +20,8 @@ export default function LoginForm() {
     const dispatch = useDispatch();
     const navigation = useNavigation();
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
 
     const [request, response, promptAsync] = Google.useAuthRequest({
         expoClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
@@ -32,63 +34,67 @@ export default function LoginForm() {
             setLoading(false);
         }, time);
     }
-    React.useEffect(() => {
-        // Check if user is already logged in
-        AsyncStorage.getItem(USER_KEY)
-            .then(userString => {
-                if (userString) {
-                    const user = JSON.parse(userString);
-                    if (user?.id != null && user?.id != undefined) {
-                        dispatch({ type: 'LOGIN', user });
-                        navigation.navigate('Dashboard');
-                    }
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
 
+    React.useEffect(() => {
         if (response?.type === 'success') {
             setLoading(true);
             const { accessToken } = response.authentication;
 
-            axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            })
-                .then(response => {
+            axios
+                .get("https://www.googleapis.com/oauth2/v1/userinfo", {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                })
+                .then((response) => {
                     const user = response.data;
-                    dispatch({ type: 'LOGIN', user });
+                    dispatch({ type: "LOGIN", user });
                     // Store user in local storage
                     AsyncStorage.setItem(USER_KEY, JSON.stringify(user))
-                        .catch(error => {
+                        .catch((error) => {
                             console.error(error);
                         });
-                    navigation.navigate('Dashboard');
+                    navigation.navigate("Dashboard");
                     hideLoading(2000);
                 })
-                .catch(error => {
-                    console.error(error);
+                .catch((error) => {
+                    setError(error);
+                    hideLoading(1000);
                 });
         }
     }, [response]);
 
     return (
-        <View>
+        <View style={{ alignItems: "center" }}>
             <ActivityModal loading={loading} />
-            <TouchableOpacity disabled={!request}
+            <TouchableOpacity
+                disabled={!request}
                 title="Login"
                 onPress={() => {
                     promptAsync();
                 }}
-                style={{ padding: 10, alignItems: 'center' }}
+                style={{ padding: 10, alignItems: "center" }}
             >
                 <Image
-                    style={{ height: 50, width: 240, marginTop: 10, borderColor: 'black', borderWidth: 1, borderRadius: 5 }}
+                    style={{
+                        height: 50,
+                        width: 240,
+                        marginTop: 10,
+                        borderColor: "black",
+                        borderWidth: 1,
+                        borderRadius: 5,
+                    }}
                     source={GoogleButton}
                 />
             </TouchableOpacity>
+            {error && (
+                <View>
+                    <Text style={{ color: "red" }}>
+                        An error occurred: {error.message}
+                    </Text>
+                </View>
+
+            )}
         </View>
     );
 }
